7d99e0622ba0510b1aa42cb69cca08bb24c603ff
LuninDima <moon-di-2009@yandex.ru>
2021-08-17 17:49:32 +0300

    1. создание observer для viewModel

--- app/src/main/java/ru/moondi/kotlinweather/view/MainFragment.kt
+++ app/src/main/java/ru/moondi/kotlinweather/view/MainFragment.kt
@@ -4,19 +4,34 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import ru.moondi.kotlinweather.R
+import ru.moondi.kotlinweather.viewmodel.MainViewModel
 
 class MainFragment : Fragment() {
-    companion object{
-        fun newInstance()= MainFragment()
-        }
+
+    lateinit var viewModel: MainViewModel
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-       return inflater.inflate(R.layout.main_fragment, container, false)
-           }
+        return inflater.inflate(R.layout.main_fragment, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        val observer = Observer<Any>{
+            Toast.makeText(context,"Тест", Toast.LENGTH_LONG).show()}
+        viewModel.getLiveData().observe(viewLifecycleOwner, observer)
+    }
+}
