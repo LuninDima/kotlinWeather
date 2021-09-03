0362141d154d42980939bdf1e7fea07405ad89d6
LuninDima <moon-di-2009@yandex.ru>
2021-09-03 18:53:44 +0300

    Измение типа данных для состояния приложения

--- app/src/main/java/ru/moondi/kotlinweather/viewmodel/AppState.kt
+++ app/src/main/java/ru/moondi/kotlinweather/viewmodel/AppState.kt
@@ -3,7 +3,7 @@ package ru.moondi.kotlinweather.viewmodel
 import ru.moondi.kotlinweather.view.Weather
 
 sealed class AppState {
-    data class Succes(val dataWeather: Weather) : AppState()
+    data class Succes(val dataWeather: List<Weather>) : AppState()
     data class Error(val error: Throwable) : AppState()
     object Loading : AppState()
 }
